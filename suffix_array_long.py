9d8b04b11e09e3ead41bc5a7037c750ad957bdc9
Mathuba <Mathuba58@gmail.com>
2019-08-30 15:32:02 +0200

    Added code for sort_characters function

--- suffix_array_long.py
+++ suffix_array_long.py
@@ -1,20 +1,43 @@
 # python3
 import sys
 
+CH_LEN = 256
+
+
+def sort_characters(text_str):
+    ts_len = len(text_str)
+    counts = [0 for i in range(CH_LEN)]
+    order = [None for i in range(ts_len)]
+
+    # get the total number of individual items
+    for char in text_str:
+        counts[ord(char)] += 1
+
+    # recompute starting positions in array to be returned (order)
+    for j, _ in enumerate(counts):
+        counts[j] = counts[j] + counts[j - 1]
+
+    # load reversed characters (as ints) into order array
+    for i in range(ts_len-1, -1, -1):
+        c = ord(text_str[i])
+        counts[c] = counts[c] - 1
+        order[counts[c]] = i
+    return order
+
 
 def build_suffix_array(text):
-  """
+    """
   Build suffix array of the string text and
   return a list result of the same length as the text
   such that the value result[i] is the index (0-based)
   in text where the i-th lexicographically smallest
   suffix of text starts.
   """
-  result = []
-  # Implement this function yourself
-  return result
+    result = []
+    # Implement this function yourself
+    return result
 
 
 if __name__ == '__main__':
-  text = sys.stdin.readline().strip()
-  print(" ".join(map(str, build_suffix_array(text))))
+    text = sys.stdin.readline().strip()
+    print(" ".join(map(str, build_suffix_array(text))))
--- tests/test_suffix_array_long.py
+++ tests/test_suffix_array_long.py
@@ -7,3 +7,22 @@ def test_sort_characters():
     order = suffix_array_long.sort_characters(text)
     assert order == [6, 0, 2, 4, 5, 1, 3]
 
+
+def test_sort_characters_genome_one():
+    text = "AAA$"
+    order = suffix_array_long.sort_characters(text)
+    assert order == [3, 0, 1, 2]
+
+
+def test_sort_characters_genome_two():
+    text = "GAC$"
+    order = suffix_array_long.sort_characters(text)
+    assert order == [3, 1, 2, 0]
+
+
+def test_sort_characters_genome_three():
+    text = "AACGATAGCGGTAGA$"
+    order = suffix_array_long.sort_characters(text)
+    assert order == [15, 0, 1, 4, 6, 12, 14, 2, 8, 3, 7, 9, 10, 13, 5, 11]
+
+
