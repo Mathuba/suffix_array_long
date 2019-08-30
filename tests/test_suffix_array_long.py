377528f44bb952ac03438bcbf93fef8fb411e0e6
Mathuba <Mathuba58@gmail.com>
2019-08-30 15:05:19 +0200

    Added code to test sort_characters function

--- tests/test_suffix_array_long.py
+++ tests/test_suffix_array_long.py
@@ -1,2 +1,9 @@
 import suffix_array_long
 import pytest
+
+
+def test_sort_characters():
+    text = "ababaa$"
+    order = suffix_array_long.sort_characters(text)
+    assert order == [6, 0, 2, 4, 5, 1, 3]
+
